5318b56ae65812a2cf39dff247664a27a3c41e4a
Dan B <daniboot@amazon.com>
2022-05-08 20:14:29 -0600

    Updated s3randomfilelambda.py script to use Lambda environment variables

--- s3randofilelambda.py
+++ s3randofilelambda.py
@@ -1,12 +1,18 @@
+import os
 import boto3
 from datetime import date
 import random
 import string
 
+S3ObjectsToCreate = int(os.environ['S3ObjectsToCreate'])
+S3Bucket = os.environ['S3Bucket']
+S3StorageClass = os.environ['S3StorageClass']
+S3ObjectSize = int(os.environ['S3ObjectSize'])
+
 #Creating S3 Boto3 session using assumed credentials from environment
 s3 = boto3.client('s3')
 
-def generate_big_random_letters(size):
+def generate_big_random_letters():
     """
     generate big random letters/alphabets to a file
     :param filename: the filename
@@ -16,31 +22,38 @@ def generate_big_random_letters(size):
     #Generate Random Prefix
     randomprefix = ''.join([random.choice(string.ascii_letters) for i in range(10)])
     
-    for x in range (10):
+    #Run loop for how many objects should be created. Object count defined in environment variable
+    for x in range (S3ObjectsToCreate):
         #Generate Random File Name and Date
         randomfilename = ''.join([random.choice(string.ascii_letters) for i in range(10)])
         filedate = str(date.today())
         filename = filedate + "_random_file_" + randomfilename
         
-        #Generate File Data
-        chars = ''.join([random.choice(string.ascii_letters) for i in range(size)]) #1
+        #Generate File Data, object size is defined in environment variable
+        chars = ''.join([random.choice(string.ascii_letters) for i in range(S3ObjectSize)]) #1
         objectpath = "filegenerator2/" + filedate + "_" + randomprefix + "/" + filename + ".txt"
-        print(objectpath)
+        print("Writing S3 object" + objectpath)
                 
-        #Upload object to S3
+        #Upload object to S3, bucket and storage class defined in environment variables
         object = s3.put_object(
-            Bucket='BUCKETNAME',
-            Body=chars,
-            Key=objectpath,
-            StorageClass='INTELLIGENT_TIERING'
+            #Bucket='s3-storage-class-testing-912688296143',
+            Bucket = S3Bucket,
+            Body = chars,
+            Key = objectpath,
+            StorageClass = S3StorageClass
         )
     else:
         print("Done")
     
 def lambda_handler(event, context):
     print(event)
-    print(context)
-    generate_big_random_letters(1024*1024)
+    print("Printing Lambda Environment Variables:")
+    print("S3 Bucket: " + os.environ['S3Bucket'])
+    print("S3 Storage Class: " + os.environ['S3StorageClass'])
+    print("S3 Object Count: " + os.environ['S3ObjectsToCreate'])
+    print("S3 Object Size (bytes): " + os.environ['S3ObjectSize'])
+
+    generate_big_random_letters()
     return {
-        'message' : "done"
+        'message' : "Done"
     }
